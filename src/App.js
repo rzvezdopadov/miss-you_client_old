fb4f8c2864fd4b9277f7c3a021eba18b8f6e0f4f
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-02 13:11:01 +0300

    connect component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { AppHeader } from './components/AppHeader/AppHeader';
+import { AppMain } from './components/AppMain/AppMain';
+import { AppFooter } from './components/AppFooter/AppFooter';
 
 function App() {
   return (
     <div className="App">
-      
+      <AppHeader />
+      <AppMain />
+      <AppFooter />
     </div>
   );
 }
