cd31f34ee3bc7c3799ddbb886df3100ce93965cd
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-03 20:42:02 +0300

    created routes for main section

--- src/components/AppMain/AppMain.jsx
+++ src/components/AppMain/AppMain.jsx
@@ -1,7 +1,21 @@
-export function AppMain() {
-  return (
-    <div className="App-Main">
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { AboutUs } from '../AboutUs/AboutUs';
+import { Agreement } from '../Agreement/Agreement';
+import { FormEnter } from '../FormEnter/FormEnter';
+import { FormRegistration } from '../FormRegistrarion/FormRegistration';
+import { Partners } from '../Partners/Partners';
 
-    </div>
-  );
+export function AppMain() {
+    return (
+        <div className="App-Main flex flex-grow p-4 justify-center items-center">
+            <Routes>
+                <Route path="/enter" element={ <FormEnter /> } />
+                <Route path="/about" element={ <AboutUs /> } />
+                <Route path="/partners" element={ <Partners /> } />
+                <Route path="/agreement" element={ <Agreement /> } />
+                <Route path="/" element={ <FormRegistration /> } />
+            </Routes>
+        </div>
+    );
 }
