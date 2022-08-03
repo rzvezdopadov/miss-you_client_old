36de51f95452c86ed2d7c00cd53c90718e92fa73
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-03 20:37:29 +0300

    added base page partners

---
+++ src/components/Partners/Partners.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function Partners() {
+  return (
+    <div className="flex w-full justify-center ">
+        <div className="bg-gray-700 text-neutral-50 shadow-md rounded-3xl px-8 pt-2 pb-2 w-80">
+            Сотрудничество с нами
+        </div>
+    </div>
+  );
+}
