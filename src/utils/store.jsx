d03735cf280c97b8baa9968854de8030b3907ab0
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-06 00:36:56 +0300

    added new redusers to store

--- src/utils/store.jsx
+++ src/utils/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
-import { jwtReducer } from "./reducers"
+import { jwtReducer, myVaporsReducer, profileReducer } from "./reducers"
 
 export const store = configureStore({
     reducer: {
-        jwt: jwtReducer,    
+        jwt: jwtReducer,
+        profile: profileReducer,
+        myVapors: myVaporsReducer,       
     }
 })
