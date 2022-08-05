f5ec892ef56619b4a9c40b33c8aad3812072e55c
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-06 00:35:44 +0300

    fixed link logout and parameter function get state

--- src/components/AppHeaderRightSideBar/AppHeaderRightSideBar.jsx
+++ src/components/AppHeaderRightSideBar/AppHeaderRightSideBar.jsx
@@ -1,7 +1,7 @@
 import { React } from 'react';
 import { Link } from "react-router-dom";
-import { JWT_TOKEN } from '../../utils/reducers';
 import { store } from "../../utils/store";
+import { logout } from '../logout/logout';
 
 function AppHeaderRightSideBarNoAuth() {
     return (
@@ -20,13 +20,13 @@ function AppHeaderRightSideBarAuth() {
             <Link to="/vapors" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Мои пары</Link>
             <Link to="/searchvapors" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Искать людей</Link>
             <Link to="/settings" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Настройки</Link>    
-            <Link to="/logout" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Выйти</Link>    
+            <Link to="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" onClick={ logout }>Выйти</Link>    
         </>
     );
 }
 
 export function AppHeaderRightSideBar() {
-    const { jwt } = store.getState(JWT_TOKEN);
+    const { jwt } = store.getState();
 
     return (
         <div className="flex space-x-4">
