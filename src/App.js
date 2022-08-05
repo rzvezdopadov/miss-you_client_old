f51b175bc7cc86b2baf4e97c1210bd11007c361b
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-06 00:32:09 +0300

    intagrate modal loading to App

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { AppHeader } from './components/AppHeader/AppHeader';
 import { AppMain } from './components/AppMain/AppMain';
 import { AppFooter } from './components/AppFooter/AppFooter';
 import { MobileMenu } from './components/MobileMenu/MobileMenu';
+import { ModalLoading } from './components/ModalLoading/ModalLoading';
 
 function App() {
-  return (
-    <div className="App">
-        <AppHeader />
-        <AppMain />
-        <AppFooter />
-        <MobileMenu />
-    </div>
-  );
+    return (
+        <div className="App">
+            <AppHeader />
+            <AppMain />
+            <AppFooter />
+            <MobileMenu />
+            <ModalLoading />
+        </div>
+    );
 }
 
 export default App;
