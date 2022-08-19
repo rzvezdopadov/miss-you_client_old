f01e27fc229a31197bdfb4188f26cff03a209aae
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-19 14:38:37 +0300

    connect new futering in search wapors

--- src/components/SearchVapors/SearchVapors.tsx
+++ src/components/SearchVapors/SearchVapors.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
+import { UserProfile } from '../UserProfile/UserProfile';
+import { UserProfileFilters } from '../UserProfileFilters/UserProfileFilters';
+import { UserProfileShortWrapper } from '../UserProfileShortWrapper/UserProfileShortWrapper';
 
 export function SearchVapors() {
-  return (
-    <div className="flex w-full justify-center ">
-        <div className="bg-gray-700 text-neutral-50 shadow-md rounded-3xl px-8 pt-2 pb-2 w-80">
-            Поиск пары
+    return (
+        <div className="flex h-full w-full justify-center">
+            <div className="flex bg-gray-700 text-neutral-50 flex-col shadow-md rounded-3xl px-8 pt-2 pb-2 w-auto">
+                <div className="flex justify-center">
+                Поиск пары
+                </div>
+                <div className="flex justify-center">
+                    <UserProfileFilters />
+                </div>
+                <div className="flex flex-row flex-wrap justify-center">
+                    <UserProfileShortWrapper />
+                </div>
+                <UserProfile />
+            </div>
         </div>
-    </div>
-  );
+    );
 }
