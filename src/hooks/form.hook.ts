d57e7b3f5f42e6246dc2bde056d5abe861d49ab8
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-19 14:24:43 +0300

    added custom hooks for form fields

---
+++ src/hooks/form.hook.ts
@@ -0,0 +1,19 @@
+import * as React from 'react'
+
+export const useFormFieldInputString = (initialValue: string = '') => {
+    const [value, setValue] = React.useState(initialValue);
+    const onChange = React.useCallback((e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value), []);
+    return { value, onChange }    
+}
+
+export const useFormFieldSelectNumber = (initialValue: number = 0) => {
+    const [value, setValue] = React.useState(initialValue);
+    const onChange = React.useCallback((e: React.ChangeEvent<HTMLSelectElement>) => setValue(Number(e.target.value)), []);
+    return { value, onChange }    
+}
+
+export const useFormFieldSelectString = (initialValue: string = '') => {
+    const [value, setValue] = React.useState(initialValue);
+    const onChange = React.useCallback((e: React.ChangeEvent<HTMLSelectElement>) => setValue(e.target.value), []);
+    return { value, onChange }    
+}
