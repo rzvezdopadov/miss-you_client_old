031a8d64c5936461ece02daf9fb35585b8f8d1d9
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-19 14:25:20 +0300

    added interface filters for users

---
+++ src/interfaces/ifilters.ts
@@ -0,0 +1,10 @@
+export interface IFilterUsers {
+    location: string,
+    ageStart: number,
+    ageEnd: number,
+    signZodiac: number,
+    genderVapor: number,
+    religion: number,
+    smoke: number,
+    alcohol: number,
+}
