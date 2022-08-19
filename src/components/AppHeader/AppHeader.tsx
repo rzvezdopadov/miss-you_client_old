54fc8edf2e78c71a22a35eecbd3840593d21d2af
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-19 14:34:16 +0300

    fixed tabulation in file

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -7,18 +7,18 @@ import { openMobileMenu } from '../MobileMenu/MobileMenu';
 
 
 export function AppHeader() {
-  return (
-    <div className="flex flex-shrink-0 items-center justify-around bg-gray-800 h-16">
-        <div className="flex items-center">
-            <a href='https://miss-you.ru'>
-                <img className="block h-12 w-auto" src={ logoNavBar } alt="Logo" /> 
-            </a>
-            <img className="visible h-8 ml-2 w-auto" src={ logoNameNavBar } alt="LogoName" />
+    return (
+        <div className="flex flex-shrink-0 items-center justify-around bg-gray-700 h-16">
+            <div className="flex items-center">
+                <a href='https://miss-you.ru'>
+                    <img className="block h-12 w-auto" src={ logoNavBar } alt="Logo" /> 
+                </a>
+                <img className="visible h-8 ml-2 w-auto" src={ logoNameNavBar } alt="LogoName" />
+            </div>
+            <img className="visible md:hidden cursor-pointer h-8 ml-2 w-auto" src={ menuOpenNavBar } onClick={ openMobileMenu } alt="LogoMenu" />  
+            <div className="hidden md:block md:ml-6">
+                <AppHeaderRightSideBar />
+            </div> 
         </div>
-        <img className="visible md:hidden cursor-pointer h-8 ml-2 w-auto" src={ menuOpenNavBar } onClick={ openMobileMenu } alt="LogoMenu" />  
-        <div className="hidden md:block md:ml-6">
-            <AppHeaderRightSideBar />
-        </div> 
-    </div>
-  );
+    );
 }
