95d31990b9c955588d5de91ca0dce75d20024d11
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-08-27 12:24:06 +0300

    fixed layout and bl short profile

--- src/components/UserProfileShort/UserProfileShort.tsx
+++ src/components/UserProfileShort/UserProfileShort.tsx
@@ -1,32 +1,26 @@
-import { profile } from 'console';
 import * as React from 'react';
 import { IProfile } from '../../interfaces/iprofiles';
-
-function UserProfileShortInterest({ interest }) {
-    return ( 
-        <>
-            <div className="flex bg-gray-300 select-none text-sm text-black p-1 m-1 h-7 rounded-lg">
-                { interest }
-            </div>
-        </>
-    );
-}
+import { openUserProfile } from '../UserProfile/UserProfile';
+import { UserProfileInterest } from '../UserProfileInterest/UserProfileInterest';
 
 export function UserProfileShort(params: { key: string, profile: IProfile }) {
     const { profile } = params;
 
-    return (
-        <div className="flex justify-center flex-row bg-gray-900 text-neutral-50 shadow-md rounded-xl m-2 px-2 pt-2 pb-2 max-h-30 h-30 w-80" >
-            <div className="flex flex-col justify-center" >
+    const openProfileHandler = () => {
+        openUserProfile(profile);
+    }
 
+    return (
+        <div className="flex justify-center shadow-[0px_0px_1px_1px] shadow-lime-300 flex-row bg-gray-900 text-neutral-50 rounded-xl m-2 px-2 pt-2 pb-2 max-h-30 h-30 w-80" >
+            <div className="flex flex-col justify-center" >
                 <div 
                     style={{ 
-                        'background-image': 'URL(' + profile.photoLink[profile.photoMain] + ')' 
+                        backgroundImage: 'URL(' + profile.photoLink[profile.photoMain] + ')' 
                     }} 
-                    className= "flex bg-center bg-contain bg-no-repeat justify-center text-neutral-50 rounded-3xl m-1 h-32 w-32"
+                    className= "flex bg-center bg-cover bg-no-repeat justify-center shadow-[0px_0px_2px_2px] shadow-lime-300 text-neutral-50 rounded-3xl m-1 h-32 w-32"
                 >
                 </div>
-                <div className="flex bg-lime-700 justify-center cursor-pointer m-1 rounded-md">
+                <div onClick={ openProfileHandler } className="flex bg-lime-700 justify-center cursor-pointer m-1 rounded-md">
                     Посмотреть
                 </div>
             </div>
@@ -40,11 +34,9 @@ export function UserProfileShort(params: { key: string, profile: IProfile }) {
                 </div>
                 <div className="flex items-center justify-center overflow-hidden flex-wrap text-sm text-neutral-50 h-28">
                     {
-                        profile.interests.length 
-                        ? profile.interests.map((interest, i) => {
-                            return <UserProfileShortInterest key={ interest + i } interest={ interest } />
-                        }) 
-                        :<UserProfileShortInterest key={ 'interest' + profile.id } interest={ 'Отсутствуют' } />
+                        profile.interests.length ? profile.interests.map((interest, i) => {
+                            return <UserProfileInterest key={ interest + i } value={ interest } />
+                        }) :<UserProfileInterest key={ 'interest' + profile.id } value={ 'Отсутствуют' } />
                     }
                 </div>
             </div>
