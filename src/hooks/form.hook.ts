98f87664f25d2182a88e0025821086bda11c24c1
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-09-06 00:17:30 +0300

    added hook for query textaria

--- src/hooks/form.hook.ts
+++ src/hooks/form.hook.ts
@@ -6,6 +6,12 @@ export const useFormFieldInputString = (initialValue: string = '') => {
     return { value, onChange }    
 }
 
+export const useFormFieldTextAreaString = (initialValue: string = '') => {
+    const [value, setValue] = React.useState(initialValue);
+    const onChange = React.useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => setValue(e.target.value), []);
+    return { value, onChange }    
+}
+
 export const useFormFieldSelectNumber = (initialValue: number = 0) => {
     const [value, setValue] = React.useState(initialValue);
     const onChange = React.useCallback((e: React.ChangeEvent<HTMLSelectElement>) => setValue(Number(e.target.value)), []);
