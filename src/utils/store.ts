663cf659b73647feab37e9dfaba0780427aeda85
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-09-10 10:59:43 +0300

    change store redusers

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,16 +1,16 @@
 // @ts-ignore
 import { configureStore } from "@reduxjs/toolkit"
-import { filtersUserReducer, jwtReducer, modalLoadingReducer, modalMessageReducer, myVaporsReducer, profileReducer, usersProfilesReducer, userProfileReducer } from "./reducers"
+import { filtersUserReducer, jwtReducer, modalLoadingReducer, modalMessageReducer, myVaporsReducer, usersProfilesReducer, userProfileReducer, userMyProfileReducer } from "./reducers"
 
 export const store = configureStore({
     reducer: {
         modalLoading: modalLoadingReducer,
         modalMessage: modalMessageReducer,
         jwt: jwtReducer,
-        profile: profileReducer,
         myVapors: myVaporsReducer,
         filtersUser: filtersUserReducer,
         usersProfiles: usersProfilesReducer, 
-        userProfile: userProfileReducer,     
+        userProfile: userProfileReducer,
+        userMyProfile: userMyProfileReducer,    
     }
 })
