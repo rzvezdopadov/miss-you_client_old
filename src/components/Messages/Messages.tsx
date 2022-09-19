256d4d331a3ccad0a161a57940d4176545cd984f
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-09-19 22:45:11 +0300

    added selection messages

---
+++ src/components/Messages/Messages.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export function Messages() {
+  return (
+    <div className="flex w-full justify-center ">
+        <div className="bg-gray-700 text-neutral-50 shadow-md rounded-3xl px-8 pt-2 pb-2 w-80">
+            Сообщения
+        </div>
+    </div>
+  );
+}
