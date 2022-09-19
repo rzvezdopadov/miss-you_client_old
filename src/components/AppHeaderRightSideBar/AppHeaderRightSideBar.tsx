b84210eaa0bae9e6f02305f84c9bce7b5183546a
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-09-19 22:47:13 +0300

    added png for selections

--- src/components/AppHeaderRightSideBar/AppHeaderRightSideBar.tsx
+++ src/components/AppHeaderRightSideBar/AppHeaderRightSideBar.tsx
@@ -2,14 +2,28 @@ import * as React from 'react';
 import { Link } from "react-router-dom";
 import { store } from "../../utils/store";
 import { logout } from '../logout/logout';
+import message from '../../img/message.png'
+import heart from '../../img/heart.png'
+import glass from '../../img/glass.png'
+import gear from '../../img/gear.png'
+import exit from '../../img/exit.png'
+
 
 function AppHeaderRightSideBarNoAuth() {
     return (
         <>
-            <Link to="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Главная</Link>
-            <Link to="/about" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">О нас</Link>
-            <Link to="/partners" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Партнерство</Link>    
-            <Link to="/enter" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Войти</Link>
+            <Link to="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                Главная
+            </Link>
+            <Link to="/about" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                О нас
+            </Link>
+            <Link to="/partners" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                Партнерство
+            </Link>    
+            <Link to="/enter" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                Войти
+            </Link>
         </>
     );
 }
@@ -17,10 +31,21 @@ function AppHeaderRightSideBarNoAuth() {
 function AppHeaderRightSideBarAuth() {
     return (
         <>
-            <Link to="/vapors" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Мои пары</Link>
-            <Link to="/searchvapors" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Искать людей</Link>
-            <Link to="/settings" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Настройки</Link>    
-            <Link to="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" onClick={ logout }>Выйти</Link>    
+            <Link to="/vapors" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                <img className="block h-5 w-auto" src={ heart } alt="Кто лайкнул" title='Кто лайкнул'/> 
+            </Link>
+            <Link to="/messages" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                <img className="block h-5 w-auto" src={ message } alt="Сообщения" title='Сообщения'/> 
+            </Link>
+            <Link to="/searchvapors" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                <img className="block h-5 w-auto" src={ glass } alt="Поиск людей" title='Поиск людей' /> 
+            </Link>
+            <Link to="/settings" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                <img className="block h-5 w-auto" src={ gear } alt="Настройки" title='Настройки' /> 
+            </Link>    
+            <Link to="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" onClick={ logout }>
+                <img className="block h-5 w-auto" src={ exit } alt="Выход" title='Выход' /> 
+            </Link>    
         </>
     );
 }
--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
 import { Link } from "react-router-dom";
 import { store } from "../../utils/store";
+import message from '../../img/message.png'
+import heart from '../../img/heart.png'
+import glass from '../../img/glass.png'
+import gear from '../../img/gear.png'
+import exit from '../../img/exit.png'
 
 function MobileMenuNoAuth() {  
     return (
         <>
             <a href="/" className="text-right cursor-pointer text-4xl" onClick={ closeMobileMenu }>&times;</a>
-            <Link to="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Главная</Link>
-            <Link to="/about" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">О нас</Link>
-            <Link to="/partners" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Партнерство</Link>    
-            <Link to="/enter" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Войти</Link>      
+            <Link to="/" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                Главная
+            </Link>
+            <Link to="/about" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                О нас
+            </Link>
+            <Link to="/partners" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                Партнерство
+            </Link>    
+            <Link to="/enter" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                Войти
+            </Link>      
         </>
     );
 }
@@ -18,10 +31,21 @@ function MobileMenuAuth() {
     return (
         <>
             <a href="/" className="text-right cursor-pointer text-4xl" onClick={ closeMobileMenu }>&times;</a>
-            <Link to="/vapors" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Мои пары</Link>
-            <Link to="/searchvapors" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Искать людей</Link>
-            <Link to="/settings" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Настройки</Link>    
-            <Link to="/logout" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Выйти</Link>    
+            <Link to="/vapors" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                <img className="block h-5 w-auto" src={ heart } alt="Кто лайкнул" title='Кто лайкнул'/> 
+            </Link>
+            <Link to="/messages" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                <img className="block h-5 w-auto" src={ message } alt="Сообщения" title='Сообщения'/>  
+            </Link>
+            <Link to="/searchvapors" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                <img className="block h-5 w-auto" src={ glass } alt="Поиск людей" title='Поиск людей' /> 
+            </Link>
+            <Link to="/settings" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                <img className="block h-5 w-auto" src={ gear } alt="Настройки" title='Настройки' /> 
+            </Link>    
+            <Link to="/logout" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
+                <img className="block h-5 w-auto" src={ exit } alt="Выход" title='Выход' />
+            </Link>
         </>   
     );
 }
@@ -30,7 +54,7 @@ export function openMobileMenu() {
     let mobileMenu = document.getElementById('mobile-menu');
     if (!mobileMenu) return;
 
-    mobileMenu.classList.remove('left-[-250px]');
+    mobileMenu.classList.remove('left-[-100px]');
     mobileMenu.classList.add('left-0');
 }
 
@@ -41,14 +65,14 @@ function closeMobileMenu(event) {
     if (!mobileMenu) return;
 
     mobileMenu.classList.remove('left-0');
-    mobileMenu.classList.add('left-[-250px]');
+    mobileMenu.classList.add('left-[-100px]');
 }
 
 export function MobileMenu() { 
     const { jwt } = store.getState();
 
     return (
-        <div id="mobile-menu" onClick={ closeMobileMenu } className="fixed top-0 left-[-250px] w-[240px] h-screen z-50 bg-gray-700 p-5 flex flex-col space-y-5 text-white duration-300">
+        <div id="mobile-menu" onClick={ closeMobileMenu } className="fixed items-center top-0 left-[-100px] w-[100px] h-screen z-50 bg-gray-700 p-5 flex flex-col space-y-5 text-white duration-300">
             { jwt ? <MobileMenuAuth /> : <MobileMenuNoAuth /> } 
         </div>
     );
