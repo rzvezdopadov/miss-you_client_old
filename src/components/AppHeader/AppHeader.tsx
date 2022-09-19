8bdfc34ad9e6d9e66949220911feeb99cd4fe42e
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-09-19 22:46:48 +0300

    fixed logo rounded

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -11,7 +11,7 @@ export function AppHeader() {
         <div className="flex flex-shrink-0 items-center justify-around bg-gray-700 h-16">
             <div className="flex items-center">
                 <a href='https://miss-you.ru'>
-                    <img className="block h-12 w-auto" src={ logoNavBar } alt="Logo" /> 
+                    <img className="block h-12 rounded-full w-auto" src={ logoNavBar } alt="Logo" /> 
                 </a>
                 <img className="visible h-8 ml-2 w-auto" src={ logoNameNavBar } alt="LogoName" />
             </div>
