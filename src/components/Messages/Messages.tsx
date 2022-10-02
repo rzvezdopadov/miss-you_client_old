72d79344005cf34a26e151461a7c789f7eee3002
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-10-02 22:51:38 +0300

    fixed layout

--- src/components/Messages/Messages.tsx
+++ src/components/Messages/Messages.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
 export function Messages() {
-  return (
-    <div className="flex w-full justify-center ">
-        <div className="bg-gray-700 text-neutral-50 shadow-md rounded-3xl px-8 pt-2 pb-2 w-80">
-            Сообщения
+    return (
+        <div className="flex h-full w-full justify-center">
+            <div className="flex relative bg-gray-700 text-neutral-50 flex-col shadow-md rounded-3xl px-8 pt-2 pb-2 w-full">
+                <div className="flex justify-center">
+                    Сообщения
+                </div>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
