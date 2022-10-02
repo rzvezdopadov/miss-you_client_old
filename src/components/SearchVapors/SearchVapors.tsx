c1eab7a736c09a58d238242df36a954492f17035
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-10-02 22:51:59 +0300

    change tabulation

--- src/components/SearchVapors/SearchVapors.tsx
+++ src/components/SearchVapors/SearchVapors.tsx
@@ -8,7 +8,7 @@ export function SearchVapors() {
         <div className="flex h-full w-full justify-center">
             <div className="flex relative bg-gray-700 text-neutral-50 flex-col shadow-md rounded-3xl px-8 pt-2 pb-2 w-full">
                 <div className="flex justify-center">
-                Поиск пары
+                    Поиск пары
                 </div>
                 <div className="flex justify-center">
                     <UserProfileFilters />
