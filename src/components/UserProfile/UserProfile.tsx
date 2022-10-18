c07b0ff242172ffa86d481912835182933305aea
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-10-18 23:05:04 +0300

    delete space

--- src/components/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile.tsx
@@ -35,8 +35,6 @@ export function UserProfile() {
     return (
         <div ref={ refUserProfile } className="flex flex-col fixed justify-start bg-gray-900 shadow-[0px_0px_5px_5px] shadow-lime-300 text-neutral-50 rounded-xl  overflow-y-scroll lg:overflow-auto top-0 bottom-0 left-0 right-0 m-auto px-2 pt-2 z-20 pb-2 h-full lg:h-2/3 lg:max-w-5xl" >
             <div className='flex justify-center h-6 w-full'>
-                
-
                 <div onClick={ closeUserProfileHandler } className='flex justify-center absolute right-2 cursor-pointer rounded-full bg-red-400 h-6 w-6'>X</div>
             </div>
 
