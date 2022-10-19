ed9fd98d906b7efe8c21b0b6fec6cb915dc311ae
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-10-19 22:13:07 +0300

    added dialog short component for dialogs list

---
+++ src/components/DialogShort/DialogShort.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { IDialog } from '../../interfaces/iprofiles';
+
+export function DialogShort( dialog: IDialog ) {
+    return (
+        <div key={ dialog.timecode + dialog.age } className='flex items-center my-1 w-auto h-16 bg-gray-700 rounded-xl shadow-[0px_0px_1px_1px] shadow-lime-300 cursor-pointer' >
+            <div 
+                style={{ 
+                    backgroundImage: 'URL(' + dialog.photolink[dialog.photomain] + ')' 
+                }} 
+                className= "flex flex-shrink-0 bg-center bg-cover bg-no-repeat justify-center shadow-[0px_0px_2px_2px] shadow-lime-300 text-neutral-50 rounded-full m-1 h-14 w-14"
+            ></div>
+            <div className='flex flex-col w-40 h-full m-1 overflow-hidden'>
+                <div className='flex flex-shrink-0 justify-center w-max overflow-hidden select-none'>
+                    { dialog.name + ', ' + dialog.age }
+                </div>
+                <div className='flex justify-center text-zinc-400 text-sm overflow-hidden select-none'>
+                    { dialog.messages[dialog.messages.length - 1].message }
+                </div>
+            </div>
+        </div>
+    );
+}
