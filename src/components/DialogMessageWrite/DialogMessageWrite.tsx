d6aaaab96670aac970bdde368de602d149f5cd1e
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-10-21 23:54:40 +0300

    create base layout for message write

---
+++ src/components/DialogMessageWrite/DialogMessageWrite.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export function DialogMessageWrite() {
+    return (
+        <div className="flex h-full w-full justify-center">
+           
+        </div>
+    );
+}
