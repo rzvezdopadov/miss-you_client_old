a229bfade40e3806a252f8d22d416db85fe6a227
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-10-29 13:37:06 +0300

    fixed layout and bl modal dialog

--- src/components/DialogModal/DialogModal.tsx
+++ src/components/DialogModal/DialogModal.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import { useEffect, useRef } from 'react';
-import { IDialog } from '../../interfaces/iprofiles';
 import { dialogModalAction } from '../../utils/reducers';
 import { store } from '../../utils/store';
+import { Dialog } from '../Dialog/Dialog';
 
-export function openDialogModal(dialog: IDialog) {
-    store.dispatch(dialogModalAction(true, dialog));
+export function openDialogModal() {
+    store.dispatch(dialogModalAction(true));
 }
 
 function closeDialogModal() {
-    store.dispatch(dialogModalAction(false, null));   
+    store.dispatch(dialogModalAction(false));   
 }
 
 export function DialogModal() {
@@ -29,12 +29,12 @@ export function DialogModal() {
     }
 
     return (
-        <div ref={ refDialogModal } className="flex flex-col fixed justify-start bg-gray-900 shadow-[0px_0px_5px_5px] shadow-lime-300 text-neutral-50 rounded-xl overflow-y-scroll lg:overflow-auto top-0 bottom-0 left-0 right-0 m-auto px-2 pt-2 z-30 pb-2 h-full lg:h-2/3 lg:max-w-5xl" >
-            <div className='flex justify-center h-6 w-full'>
-                <div onClick={ closeDialogModalHandler } className='flex justify-center absolute right-2 cursor-pointer rounded-full bg-red-400 h-6 w-6'>X</div>
+        <div ref={ refDialogModal } className="flex flex-col fixed justify-start bg-gray-900 shadow-[0px_0px_5px_5px] shadow-lime-300 text-neutral-50 rounded-xl overflow-y-scroll lg:overflow-auto top-0 bottom-0 left-0 right-0 m-auto px-2 pt-2 z-30 pb-2 h-full lg:h-3/5 lg:max-w-xl" >
+            <div className='flex absolute justify-center h-6 w-full'>
+                <div onClick={ closeDialogModalHandler } className='flex justify-center absolute right-4 cursor-pointer rounded-full bg-red-400 h-6 w-6'>X</div>
             </div>
 
-            
+            <Dialog />            
         </div>
     );
 }
