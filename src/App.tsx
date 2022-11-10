fff2d331dd650c55fd251c50eaac09fbe5240c50
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-11-10 20:36:29 +0300

    added tested code socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+//// @ts-nocheck
 import * as React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { AppHeader } from "./components/AppHeader/AppHeader";
 import { AppMain } from "./components/AppMain/AppMain";
@@ -10,9 +11,39 @@ import {
 import { ModalLoading } from "./components/ModalLoading/ModalLoading";
 import { ModalMessage } from "./components/ModalMessage/ModalMessage";
 import { store } from "./utils/store";
+// import socketIO from "socket.io-client";
+
+// const socket = socketIO();
 
 function App() {
 	const { mobileMenu } = store.getState();
+	// const [isConnected, setIsConnected] = useState(socket.connected);
+	// const [lastPong, setLastPong] = useState(null);
+
+	// useEffect(() => {
+	// 	socket.on("connect", () => {
+	// 		setIsConnected(true);
+	// 	});
+
+	// 	socket.on("disconnect", () => {
+	// 		setIsConnected(false);
+	// 	});
+
+	// 	socket.on("pong", () => {
+	// 		const date = new Date().toISOString();
+	// 		setLastPong(date as any);
+	// 	});
+
+	// 	return () => {
+	// 		socket.off("connect");
+	// 		socket.off("disconnect");
+	// 		socket.off("pong");
+	// 	};
+	// }, []);
+
+	// const sendPing = () => {
+	// 	socket.emit("ping");
+	// };
 
 	useEffect(() => {
 		document.title = "Сайт знакомств Miss-You";
@@ -25,6 +56,11 @@ function App() {
 				if (e.clientX > 100 && mobileMenu.enabled) closeMobileMenu(e);
 			}}
 		>
+			{/* <div>
+				<p>Connected: {"" + isConnected}</p>
+				<p>Last pong: {lastPong || "-"}</p>
+				<button onClick={sendPing}>Send ping</button>
+			</div> */}
 			<AppHeader />
 			<AppMain />
 			<MobileMenu />
