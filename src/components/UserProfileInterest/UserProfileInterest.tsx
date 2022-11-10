6e8d9ebe6a55d607e7be01826ee5414d1c3bf5c3
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-11-10 20:25:24 +0300

    change type variable

--- src/components/UserProfileInterest/UserProfileInterest.tsx
+++ src/components/UserProfileInterest/UserProfileInterest.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 
-export function UserProfileInterest({ value, title = "" }) {
+export function UserProfileInterest(payload: { value: string; title: string }) {
 	return (
 		<>
 			<div
 				className="flex bg-gray-300 select-none text-sm text-black p-1 m-1 h-7 rounded-lg"
-				title={title}
+				title={payload.title}
 			>
-				{value}
+				{payload.value}
 			</div>
 		</>
 	);
