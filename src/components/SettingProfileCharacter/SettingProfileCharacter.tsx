457ced62f17aa720a97d2b9b7a3cecd712905337
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-11-10 20:28:27 +0300

    change type and bl

--- src/components/SettingProfileCharacter/SettingProfileCharacter.tsx
+++ src/components/SettingProfileCharacter/SettingProfileCharacter.tsx
@@ -1,34 +1,31 @@
 import * as React from "react";
 
-export function SettingProfileCharacter({
-	id,
-	value,
-	title = "",
-	color = "shadow-lime-300",
-	check,
-	changeClbk = (id: number) => {},
+export function SettingProfileCharacter(payload: {
+	id: number;
+	value: string;
+	title: string;
+	color: string;
+	check: boolean;
+	changeClbk: (id: number) => {};
 }) {
 	return (
 		<>
 			<div
-				className={
-					"flex flex-col  shadow-[0px_0px_3px_3px] rounded-xl p-3 relative items-center min-h-12 m-2 " +
-					color
-				}
-				title={title}
+				className={`flex flex-col  shadow-[0px_0px_3px_3px] rounded-xl p-3 relative items-center min-h-12 m-2 ${payload.color}`}
+				title={payload.title}
 			>
 				<div className="flex items-center">
 					<input
 						className=" shadow-[0px_0px_2px_2px] shadow-lime-300 rounded-lg m-2"
 						onChange={() => {
-							changeClbk(id);
+							payload.changeClbk(payload.id);
 						}}
 						type={"checkbox"}
-						checked={check}
+						checked={payload.check}
 					></input>
-					<label className="flex select-none">{value}</label>
+					<label className="flex select-none">{payload.value}</label>
 				</div>
-				<div className="flex select-none">{title}</div>
+				<div className="flex select-none">{payload.title}</div>
 			</div>
 		</>
 	);
