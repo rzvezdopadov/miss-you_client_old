14ef31082abec7cbdd439457a49af62a83a32b78
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-11-14 18:29:04 +0300

    added fn for scroll

---
+++ src/utils/pagescroll.ts
@@ -0,0 +1,29 @@
+export const scrollToBottom = (bottomRef: React.RefObject<HTMLDivElement>) => {
+	if (!bottomRef.current) return;
+
+	bottomRef.current.scrollIntoView({
+		behavior: "smooth",
+		block: "start",
+	});
+};
+
+export const scrollToTop = (bottomRef: React.RefObject<HTMLDivElement>) => {
+	if (!bottomRef.current) return;
+
+	bottomRef.current.scrollTo(0, 0);
+};
+
+export const invisibleOnScrollToTop = (
+	e: React.UIEvent<HTMLDivElement>,
+	topBtnRef: React.RefObject<HTMLDivElement>
+) => {
+	const scrollPos = e.currentTarget.scrollTop;
+
+	if (!(e && topBtnRef.current)) return;
+
+	if (scrollPos > 100) {
+		topBtnRef.current.classList.remove("invisible");
+	} else {
+		topBtnRef.current.classList.add("invisible");
+	}
+};
