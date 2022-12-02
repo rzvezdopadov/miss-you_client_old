ff1c6289ba4e033a607db1ba22b47d313376feca
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2022-12-02 19:24:48 +0300

    added base component photo add

---
+++ src/components/Modal/ModalPhotoAdd/ModalPhotoAdd.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { store } from "../../../utils/store";
+
+export function ModalPhotoAdd() {
+	const { modalMessage } = store.getState();
+
+	return <div></div>;
+}
