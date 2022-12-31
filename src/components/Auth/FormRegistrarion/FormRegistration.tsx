eb5ceaa478f17dafe7e537e8399ba495311b5a89
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-01-01 02:02:57 +0300

    change form registration

--- src/components/Auth/FormRegistrarion/FormRegistration.tsx
+++ src/components/Auth/FormRegistrarion/FormRegistration.tsx
@@ -1,6 +1,27 @@
 import * as React from "react";
+import {
+	SelectFromArr,
+	SelectFromArrValue,
+} from "../../Utils/SelectFromArr/SelectFromArr";
+import { store } from "../../../utils/store";
+import {
+	arr_gender,
+	arr_genderVapor,
+	arr_growth,
+	arr_weight,
+} from "../../../arrdata/profiles";
+import { registrationAction } from "../../../utils/reducers";
+import { IRegistration } from "../../../interfaces/iprofiles";
 
 export function FormRegistration() {
+	const { registration } = store.getState();
+
+	let date = new Date();
+
+	date.setFullYear(date.getFullYear() - 18);
+
+	let minDateBirth = date.toISOString().split("T")[0];
+
 	function checkboxAgreementCheck(
 		e: React.SyntheticEvent<EventTarget>
 	): void {
@@ -12,107 +33,189 @@ export function FormRegistration() {
 		e.target ? (elemBtn.disabled = false) : (elemBtn.disabled = true);
 	}
 
+	const locationOnChangeHandler = (
+		e: React.ChangeEvent<HTMLSelectElement>
+	) => {
+		onChangeValueRegistration(e, "location", "string");
+	};
+
+	const onChangeValueRegistration = (
+		e:
+			| React.ChangeEvent<HTMLSelectElement>
+			| React.ChangeEvent<HTMLTextAreaElement>
+			| React.ChangeEvent<HTMLInputElement>,
+		key: keyof IRegistration,
+		type = "number"
+	) => {
+		const { registration } = store.getState();
+
+		const newRegistration = { ...registration };
+		let value: number | string = e.target.value;
+
+		if (type !== "string") {
+			value = Number(value);
+		}
+
+		newRegistration[key] = value as never;
+		store.dispatch(registrationAction(newRegistration));
+	};
+
+	const birhdayOnChangeHandler = (e: { target: { value: string } }) => {
+		const newRegistration = { ...registration };
+		const date: string = e.target.value;
+		const arrDate = date.split("-");
+
+		if (arrDate.length !== 3) return;
+
+		newRegistration.yearofbirth = Number(arrDate[0]);
+		newRegistration.monthofbirth = Number(arrDate[1]);
+		newRegistration.birthday = Number(arrDate[2]);
+		store.dispatch(registrationAction(newRegistration));
+	};
+
+	const growthOnChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		onChangeValueRegistration(e, "growth");
+	};
+	const weightOnChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		onChangeValueRegistration(e, "weight");
+	};
+
+	const genderOnChangeHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		onChangeValueRegistration(e, "gender");
+	};
+	const genderVaporOnChangeHandler = (
+		e: React.ChangeEvent<HTMLSelectElement>
+	) => {
+		onChangeValueRegistration(e, "gendervapor");
+	};
+	const nameOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+		onChangeValueRegistration(e, "name", "string");
+	};
+	const emailOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+		onChangeValueRegistration(e, "email", "string");
+	};
+	const passwordOnChangeHandler = (
+		e: React.ChangeEvent<HTMLInputElement>
+	) => {
+		onChangeValueRegistration(e, "password", "string");
+	};
+
 	return (
 		<div className="flex w-full justify-center min-w-xs">
-			<form
-				className="bg-gray-700 shadow-md rounded-3xl px-8 pt-2 pb-2 w-80"
-				method="POST"
-			>
-				<label className="block text-white text-2xl font-bold mb-4">
+			<div className="block bg-gray-700 shadow-md rounded-3xl px-2 pt-2 pb-2 w-80">
+				<label className="text-white text-2xl font-bold mb-4">
 					Регистрация
 				</label>
-				<div className="mb-4">
-					<div className="mb-4 relative w-64">
-						<select
-							placeholder="Имя"
-							className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-						>
-							<option value="0">Я мужчина</option>
-							<option value="1">Я женщина</option>
-						</select>
-						<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-							<svg
-								className="fill-current h-4 w-4"
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 20 20"
-							>
-								<path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-							</svg>
-						</div>
-					</div>
-					<div className="mb-4 relative w-64">
-						<select
-							placeholder="Имя"
-							className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-						>
-							<option value="0">Ищу женщину</option>
-							<option value="1">Ищу мужчину</option>
-							<option value="2">Ищу друзей</option>
-						</select>
-						<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-							<svg
-								className="fill-current h-4 w-4"
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 20 20"
-							>
-								<path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-							</svg>
-						</div>
-					</div>
-					<div className="mb-4">
-						<input
-							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-							id="username"
-							type="text"
-							placeholder="Имя"
-						/>
-					</div>
-					<div className="mb-4">
-						<input
-							className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-							id="email"
-							type="email"
-							placeholder="E-mail"
-						/>
-					</div>
-					<div className="mb-4">
-						<input
-							className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-							id="password"
-							type="password"
-							placeholder="Пароль"
-						/>
-					</div>
-					<div className="mb-4">
-						<input
-							type="checkbox"
-							onClick={checkboxAgreementCheck}
-						/>
-						<span className="text-white">
-							{" "}
-							Я подтверждаю, что мне уже исполнилось 18 лет и я
-							принимаю{" "}
-						</span>
-						<a
-							href="/agreement"
-							target="_blank"
-							className="text-red-500"
-						>
-							Пользовательское соглашение{" "}
-						</a>
-					</div>
-					<div className="flex items-center justify-center">
-						<button
-							id="btnRegistration"
-							disabled
-							className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-							type="button"
-						>
-							Найти себе пару
-						</button>
-					</div>
+
+				<SelectFromArr
+					keyOpt={"gender"}
+					value={registration.gender}
+					onChangeHandler={genderOnChangeHandler}
+					arr={arr_gender}
+					title={"Кто я?"}
+				/>
+
+				<SelectFromArr
+					keyOpt={"gendervapor"}
+					value={registration.gendervapor}
+					onChangeHandler={genderVaporOnChangeHandler}
+					arr={arr_genderVapor}
+					title={"Кого ищу?"}
+				/>
+
+				<div className="flex justify-around shadow-[0px_0px_3px_3px] shadow-lime-300 rounded-xl items-center relative p-1.5 m-3">
+					<label htmlFor="date" className="text-white">
+						{"Дата рождения:"}
+					</label>
+					&nbsp;
+					<input
+						value={
+							"" +
+							registration.yearofbirth +
+							"-" +
+							(registration.monthofbirth < 10
+								? "0" + registration.monthofbirth
+								: registration.monthofbirth) +
+							"-" +
+							(registration.birthday < 10
+								? "0" + registration.birthday
+								: registration.birthday)
+						}
+						className="border rounded select-none bg-slate-300 text-black "
+						type="date"
+						max={minDateBirth}
+						name="date"
+						onChange={birhdayOnChangeHandler}
+					/>
 				</div>
-			</form>
+
+				<div className="">
+					<SelectFromArrValue
+						keyOpt={"growth"}
+						value={registration.growth}
+						onChangeHandler={growthOnChangeHandler}
+						arr={arr_growth}
+						title={"Рост:"}
+					/>
+				</div>
+
+				<div className="flex flex-col my-1">
+					<input
+						value={registration.name}
+						onChange={nameOnChangeHandler}
+						title="Ваше имя"
+						className="flex text-center rounded-xl shadow-[0px_0px_3px_3px] shadow-lime-300 bg-slate-300 leading-tight text-black m-1 mx-3 py-2 px-3"
+						placeholder="Ваше имя"
+					/>
+				</div>
+
+				<div className="flex flex-col my-2">
+					<input
+						value={registration.email}
+						onChange={emailOnChangeHandler}
+						title="E-mail"
+						className="flex text-center rounded-xl shadow-[0px_0px_3px_3px] shadow-lime-300 bg-slate-300 leading-tight text-black m-1 mx-3 py-2 px-3"
+						type="email"
+						placeholder="E-mail"
+					/>
+				</div>
+
+				<div className="flex flex-col my-2">
+					<input
+						value={registration.password}
+						onChange={passwordOnChangeHandler}
+						title="Пароль"
+						className="flex text-center rounded-xl shadow-[0px_0px_3px_3px] shadow-lime-300 bg-slate-300 leading-tight text-black m-1 mx-3 py-2 px-3"
+						type="password"
+						placeholder="Пароль"
+					/>
+				</div>
+				<div className="mb-4">
+					<input type="checkbox" onClick={checkboxAgreementCheck} />
+					<span className="text-white">
+						{
+							" Я подтверждаю, что мне уже исполнилось 18 лет и я принимаю "
+						}
+					</span>
+					<a
+						href="/agreement"
+						target="_blank"
+						className="text-red-500"
+					>
+						{"Пользовательское соглашение"}
+					</a>
+				</div>
+				<div className="flex items-center justify-center">
+					<button
+						id="btnRegistration"
+						disabled
+						className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+						type="button"
+					>
+						Найти себе пару
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
