5bb4c7c5bcb6128d4d5c4c4713b6be2f1954d534
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-01-08 20:19:22 +0300

    added new fn

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,3 +1,18 @@
 export const randNum = (count: number) => {
 	return Math.floor(Math.random() * count);
 };
+
+let alphabet = "abdehkmnpswxzABDEFGHKMNPQRSTWXZ123456789";
+
+export function getRandomString(count: number): string {
+	let str = "";
+
+	if (!count) return str;
+
+	for (let i = 0; i < count; i++) {
+		var pos = randNum(alphabet.length);
+		str += alphabet[pos];
+	}
+
+	return str;
+}
