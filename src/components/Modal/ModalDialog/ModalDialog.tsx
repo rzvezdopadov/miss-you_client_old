50835c23122f50558981ed6ca10d4dc04187ec56
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-01-21 17:10:59 +0300

    fix layout

--- src/components/Modal/ModalDialog/ModalDialog.tsx
+++ src/components/Modal/ModalDialog/ModalDialog.tsx
@@ -39,7 +39,7 @@ export function ModalDialog() {
 	return (
 		<div
 			ref={refModalDialog}
-			className="flex flex-col fixed justify-start bg-gray-900 shadow-[0px_0px_5px_5px] shadow-lime-300 text-neutral-50 rounded-xl top-0 bottom-0 left-0 right-0 m-auto px-2 pt-2 z-30 pb-2 h-full lg:h-3/5 lg:max-w-xl"
+			className="flex flex-col invisible fixed justify-start bg-gray-900 shadow-[0px_0px_5px_5px] shadow-lime-300 text-neutral-50 rounded-xl top-0 bottom-0 left-0 right-0 m-auto px-2 pt-2 z-30 pb-2 h-full lg:h-3/5 lg:max-w-xl"
 		>
 			<div className="flex absolute justify-center h-6 w-full">
 				<div
