92af6de8d0896e774d58cc0a1842b6562bc34e61
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-01-24 23:37:53 +0300

    added base component

---
+++ src/components/Pages/Shop/ShopBuyMYPoints/ShopBuyMYPoints.tsx
@@ -0,0 +1,9 @@
+import { LabelHeaderLG } from "../../../Utils/Labels/Labels";
+
+export function ShopBuyMYPoints() {
+	return (
+		<div className="flex">
+			<LabelHeaderLG value={"Раздел в разработке"} />
+		</div>
+	);
+}
