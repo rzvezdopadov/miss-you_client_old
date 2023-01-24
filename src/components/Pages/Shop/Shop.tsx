5b6a583c8d03754dea8df0358a1db722afda85fb
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-01-24 23:41:23 +0300

    integrated chapter buy MY and change layout

--- src/components/Pages/Shop/Shop.tsx
+++ src/components/Pages/Shop/Shop.tsx
@@ -4,6 +4,7 @@ import { Button } from "../../Utils/Buttons/Buttons";
 import { ShopRating } from "./ShopRating/ShopRating";
 import { ShopStickers } from "./ShopStickers/ShopStickers";
 import { store } from "../../../utils/store";
+import { ShopBuyMYPoints } from "./ShopBuyMYPoints/ShopBuyMYPoints";
 
 enum shopPage {
 	rating,
@@ -13,17 +14,17 @@ enum shopPage {
 
 export function Shop() {
 	const { userMyProfile } = store.getState();
-	const [shop, setShop] = useState(shopPage.rating);
+	const [shop, setShop] = useState<shopPage>(shopPage.rating);
 
 	return (
 		<div className="flex h-full w-full justify-center">
-			<div className="flex overflow-y-scroll items-center relative bg-gray-700 text-neutral-50 flex-col shadow-md rounded-3xl px-8 pt-2 pb-2 w-full">
+			<div className="flex overflow-y-scroll justify-start items-center relative bg-gray-700 text-neutral-50 flex-col shadow-md rounded-3xl px-8 pt-2 pb-2 w-full">
 				<LabelHeader value={`Магазин`} />
 				<div className="w-48">
 					<Cash value={userMyProfile.cash} />
 				</div>
 
-				<div className="flex">
+				<div className="flex justify-center flex-wrap">
 					<Button
 						value={"Рейтинг"}
 						onClick={() => setShop(shopPage.rating)}
@@ -42,7 +43,7 @@ export function Shop() {
 				) : shop === shopPage.stickers ? (
 					<ShopStickers />
 				) : (
-					<></>
+					<ShopBuyMYPoints />
 				)}
 			</div>
 		</div>
