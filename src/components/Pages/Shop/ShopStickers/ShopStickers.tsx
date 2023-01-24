25d34e92dcadec71d63cc19eb5973e07d37ec5c5
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-01-24 23:40:19 +0300

    integrated new chapter

--- src/components/Pages/Shop/ShopStickers/ShopStickers.tsx
+++ src/components/Pages/Shop/ShopStickers/ShopStickers.tsx
@@ -1,10 +1,55 @@
+import { useState } from "react";
 import { LabelHeaderLG } from "../../../Utils/Labels/Labels";
+import { Button } from "../../../Utils/Buttons/Buttons";
+import { MyStickers } from "./MyStickers/MyStickers";
+import { FreeStickers } from "./FreeStickers/FreeStickers";
+import { BuyStickers } from "./BuyStickers/BuyStickers";
+
+enum shopStickersPages {
+	my,
+	free,
+	buy,
+}
 
 export function ShopStickers() {
+	const [shopStickersPage, setShopStickersPage] = useState<shopStickersPages>(
+		shopStickersPages.my
+	);
+
 	return (
-		<div className="flex flex-col justify-start items-center w-full h-full">
-			<LabelHeaderLG value={"Приобрести стикеры"} />
-			<div className="flex justify-center flex-wrap w-full"></div>
+		<div className="flex flex-col items-center w-full h-full">
+			<LabelHeaderLG value={"Cтикеры"} />
+			<div className="flex justify-center flex-row flex-wrap w-full">
+				<div className="flex">
+					<Button
+						value={"Мои"}
+						onClick={() =>
+							setShopStickersPage(shopStickersPages.my)
+						}
+					/>
+					<Button
+						value={"Бесплатные"}
+						onClick={() =>
+							setShopStickersPage(shopStickersPages.free)
+						}
+					/>
+					<Button
+						value={"Платные"}
+						onClick={() =>
+							setShopStickersPage(shopStickersPages.buy)
+						}
+					/>
+				</div>
+				<div className="flex justify-center w-full h-full">
+					{shopStickersPage === shopStickersPages.my ? (
+						<MyStickers />
+					) : shopStickersPage === shopStickersPages.free ? (
+						<FreeStickers />
+					) : (
+						<BuyStickers />
+					)}
+				</div>
+			</div>
 		</div>
 	);
 }
