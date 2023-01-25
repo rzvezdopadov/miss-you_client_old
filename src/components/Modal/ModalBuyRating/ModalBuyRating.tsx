85f6664975f844b54c4e095579bac4d57249ac83
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-01-25 22:29:25 +0300

    change layout

--- src/components/Modal/ModalBuyRating/ModalBuyRating.tsx
+++ src/components/Modal/ModalBuyRating/ModalBuyRating.tsx
@@ -67,7 +67,7 @@ export function ModalBuyRating() {
 	return (
 		<div
 			ref={refModalBuyRating}
-			className="flex flex-col fixed justify-center items-center bg-gray-900 shadow-[0px_0px_5px_5px] shadow-lime-300 text-neutral-50 rounded-xl top-0 bottom-0 left-0 right-0 m-auto px-2 pt-2 z-30 pb-2 h-36 w-80"
+			className="flex flex-col invisible fixed justify-center items-center bg-gray-900 shadow-[0px_0px_5px_5px] shadow-lime-300 text-neutral-50 rounded-xl top-0 bottom-0 left-0 right-0 m-auto px-2 pt-2 z-30 pb-2 h-36 w-80"
 		>
 			<div className="flex">{`Вы действительно хотите купить ${modalBuyRating.rate.amountRate} баллов рейтинга за ${modalBuyRating.rate.price} MY-баллов?`}</div>
 			<div className="flex justify-center h-6 w-full">
