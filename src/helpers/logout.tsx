29294e5391578be8f358c60cbe3db17ebe1c6682
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-02-04 16:43:39 +0300

    fix bug with clear state

--- src/helpers/logout.tsx
+++ src/helpers/logout.tsx
@@ -1,10 +1,26 @@
 import { getCookiesJWT } from "./cookie";
 import { store } from "../store/store";
 import { jwtAction } from "../store/redusers/auth";
+import {
+	initialStateUserMyProfile,
+	initialStateUserProfile,
+	userMyProfileAction,
+	userProfileAction,
+} from "../store/redusers/profile";
+import {
+	dialogAction,
+	dialogsAction,
+	initialStateDialog,
+	initialStateDialogs,
+} from "../store/redusers/dialog";
 
 export function logout() {
 	const jwt = getCookiesJWT();
 
 	document.cookie = `jwt=${jwt}; max-age=${-1}`;
 	store.dispatch(jwtAction(""));
+	store.dispatch(userMyProfileAction(initialStateUserMyProfile));
+	store.dispatch(userProfileAction(false, initialStateUserProfile.profile));
+	store.dispatch(dialogAction(initialStateDialog));
+	store.dispatch(dialogsAction(initialStateDialogs));
 }
