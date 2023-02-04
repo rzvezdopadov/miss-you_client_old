8af8b7be78ec92ec7456a27a4377819d0f72597a
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-02-04 17:05:47 +0300

    delete don`t used component

--- src/components/modal/ModalPhotoAdd.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { store } from "../../store/store";
-
-export function ModalPhotoAdd() {
-	const { modalMessage } = store.getState();
-
-	return <div></div>;
-}
