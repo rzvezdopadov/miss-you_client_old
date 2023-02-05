26ab5239c8b9068513127aebb62e983295720fcc
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-02-05 16:53:36 +0300

    added bl for open profile from dialog

--- src/components/widgets/dialog/DialogShort.tsx
+++ src/components/widgets/dialog/DialogShort.tsx
@@ -2,18 +2,38 @@ import * as React from "react";
 import { IDialog } from "../../../interfaces/iprofiles";
 import { messageType } from "../../../interfaces/ishop";
 import { getAgeFromYear } from "../../../helpers/age";
+import { useQueryGetProfile } from "../../../hooks/api.hook";
+import { IQueryGetProfile } from "../../../interfaces/iquery";
+import { useEffect } from "react";
+import { userProfileOpen } from "../../modal/ModalUserProfile";
+import { modalMessageOpen } from "../../modal/ModalMessage";
 
 export function DialogShort(payload: {
 	dialog: IDialog;
 	onClickHandler: React.MouseEventHandler<HTMLDivElement>;
 }) {
+	const { data, error, querySendGetProfile } = useQueryGetProfile();
+
 	const { dialog, onClickHandler } = payload;
 
+	const openProfileHandler = () => {
+		const data: IQueryGetProfile = {
+			userid: String(dialog.userid),
+		};
+
+		querySendGetProfile(data);
+	};
+
+	useEffect(() => {
+		if (data) {
+			userProfileOpen(data);
+		} else if (error) {
+			modalMessageOpen(error.response.data.message);
+		}
+	}, [data, error]);
+
 	return (
-		<div
-			className="flex items-center my-1 w-auto h-16 bg-gray-700 rounded-xl shadow-[0px_0px_1px_1px] shadow-lime-300 cursor-pointer"
-			onClick={onClickHandler}
-		>
+		<div className="flex items-center my-1 w-auto h-16 bg-gray-700 rounded-xl shadow-[0px_0px_1px_1px] shadow-lime-300 cursor-pointer">
 			<div
 				style={{
 					backgroundImage: `URL(${
@@ -21,8 +41,12 @@ export function DialogShort(payload: {
 					})`,
 				}}
 				className="flex flex-shrink-0 bg-center bg-cover bg-no-repeat justify-center shadow-[0px_0px_2px_2px] shadow-lime-300 text-neutral-50 rounded-full m-1 h-14 w-14"
+				onClick={openProfileHandler}
 			></div>
-			<div className="flex flex-col md:w-40 h-full m-1 overflow-hidden">
+			<div
+				className="flex flex-col md:w-40 h-full m-1 overflow-hidden"
+				onClick={onClickHandler}
+			>
 				<div className="flex flex-shrink-0 justify-center w-max overflow-hidden select-none">
 					{`${dialog.name}, ${getAgeFromYear(dialog.yearofbirth)}`}
 				</div>
