acb7535c7a42d01c313337a274a616c8138f0e17
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-02-11 19:15:09 +0300

    decompose fn

--- src/components/utils/Selects.tsx
+++ src/components/utils/Selects.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { IFilterParamSelect } from "../../interfaces/iprofiles";
 
 export function SelectFromArr(params: {
 	value: string | number | readonly string[];
@@ -60,6 +61,47 @@ export function SelectFromArrValue(params: {
 					})}
 				</select>
 			</div>
+		</div>
+	);
+}
+
+export function FiltersOptionAtArr(payload: {
+	headName: string;
+	arrArgs: Array<{
+		data: Array<number | string>;
+		filterParam: IFilterParamSelect;
+		valen?: boolean;
+	}>;
+}) {
+	return (
+		<div className="flex flex-row flex-wrap bg-gray-900 p-0.5 px-1 m-1.5 shadow-[0px_0px_3px_3px] shadow-lime-300 rounded-xl justify-center overflow-hidden">
+			<span className="flex m-1">{payload.headName}</span>
+
+			{payload.arrArgs.map((arg, i: number) => {
+				return (
+					<select
+						className="flex bg-gray-300 text-black text-center cursor-pointer m-1 rounded-lg"
+						key={i + payload.headName}
+						value={arg.filterParam.value}
+						onChange={arg.filterParam.onChange}
+					>
+						{arg.data.map((value, i: number) => {
+							return payload.arrArgs.length < 2 ? (
+								<option
+									key={String(i) + value}
+									value={arg.valen ? value : i}
+								>
+									{value}
+								</option>
+							) : (
+								<option key={String(i) + value} value={value}>
+									{value}
+								</option>
+							);
+						})}
+					</select>
+				);
+			})}
 		</div>
 	);
 }
