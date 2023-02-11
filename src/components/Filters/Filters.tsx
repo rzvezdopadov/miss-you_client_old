73f65703861e9dddf580f3663fa3d28b1afdaef6
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-02-11 19:14:51 +0300

    decompose fn and change type interfaces

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -10,61 +10,21 @@ import {
 	data_smoke,
 	data_weight,
 } from "../../data/profiles";
-import { IFilterParam } from "../../interfaces/iprofiles";
-
-function FiltersOptionAtArr(payload: {
-	headName: string;
-	arrArgs: Array<{
-		data: Array<number | string>;
-		filterParam: IFilterParam;
-		valen?: boolean;
-	}>;
-}) {
-	return (
-		<div className="flex flex-row flex-wrap bg-gray-900 p-0.5 px-1 m-1.5 shadow-[0px_0px_3px_3px] shadow-lime-300 rounded-xl justify-center overflow-hidden">
-			<span className="flex m-1">{payload.headName}</span>
-
-			{payload.arrArgs.map((arg, i: number) => {
-				return (
-					<select
-						className="flex bg-gray-300 text-black text-center cursor-pointer m-1 rounded-lg"
-						key={i + payload.headName}
-						value={arg.filterParam.value}
-						onChange={arg.filterParam.onChange}
-					>
-						{arg.data.map((value, i: number) => {
-							return payload.arrArgs.length < 2 ? (
-								<option
-									key={String(i) + value}
-									value={arg.valen ? value : i}
-								>
-									{value}
-								</option>
-							) : (
-								<option key={String(i) + value} value={value}>
-									{value}
-								</option>
-							);
-						})}
-					</select>
-				);
-			})}
-		</div>
-	);
-}
+import { IFilterParamSelect } from "../../interfaces/iprofiles";
+import { FiltersOptionAtArr } from "../utils/Selects";
 
 export function Filters(payload: {
-	location: IFilterParam;
-	ageStart: IFilterParam;
-	ageEnd: IFilterParam;
-	growthStart: IFilterParam;
-	growthEnd: IFilterParam;
-	weight: IFilterParam;
-	signZodiac: IFilterParam;
-	genderVapor: IFilterParam;
-	religion: IFilterParam;
-	smoke: IFilterParam;
-	alcohol: IFilterParam;
+	location: IFilterParamSelect;
+	ageStart: IFilterParamSelect;
+	ageEnd: IFilterParamSelect;
+	growthStart: IFilterParamSelect;
+	growthEnd: IFilterParamSelect;
+	weight: IFilterParamSelect;
+	signZodiac: IFilterParamSelect;
+	genderVapor: IFilterParamSelect;
+	religion: IFilterParamSelect;
+	smoke: IFilterParamSelect;
+	alcohol: IFilterParamSelect;
 }) {
 	return (
 		<>
