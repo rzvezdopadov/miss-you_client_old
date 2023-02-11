b99f85d9aff2bb28817cc81dee70946d32758275
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-02-11 19:06:42 +0300

    create new input

--- src/components/utils/Inputs.tsx
+++ src/components/utils/Inputs.tsx
@@ -1,3 +1,5 @@
+import { IFilterParamInput } from "../../interfaces/iprofiles";
+
 export function Input(payload: {
 	value: string | number;
 	onChange: any;
@@ -14,6 +16,26 @@ export function Input(payload: {
 				type={payload.type}
 				placeholder={payload.placeholder}
 			/>
+		</div>
+	);
+}
+
+export function FiltersInput(payload: {
+	headName: string;
+	filterParam: IFilterParamInput;
+}) {
+	return (
+		<div
+			key={payload.headName}
+			className="flex flex-row flex-wrap bg-gray-900 p-0.5 px-1 m-1.5 shadow-[0px_0px_3px_3px] shadow-lime-300 rounded-xl justify-center overflow-hidden"
+		>
+			<span className="flex m-1">{payload.headName}</span>
+
+			<input
+				className="flex bg-gray-300 text-black text-center m-1 rounded-lg"
+				value={payload.filterParam.value}
+				onChange={payload.filterParam.onChange}
+			></input>
 		</div>
 	);
 }
