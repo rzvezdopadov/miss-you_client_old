3b1b6d0c7ec14b566be9263d29fd2b620281d2bb
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-02-12 17:03:23 +0300

    change layout and bl

--- src/components/widgets/userprofile/UserProfileInterests.tsx
+++ src/components/widgets/userprofile/UserProfileInterests.tsx
@@ -1,29 +1,33 @@
 import * as React from "react";
 import { UserProfileInterest } from "./UserProfileInterest";
+import { IProfile } from "../../../interfaces/iprofiles";
 
-export function UserProfileInterests(payload: {
-	arrayInterests: Array<string>;
-	userId: string;
-}) {
+export function UserProfileInterests(payload: { profile: IProfile }) {
 	return (
-		<>
-			{payload.arrayInterests.length ? (
-				payload.arrayInterests.map((interest, i) => {
-					return (
+		<div className="flex flex-col justify-center">
+			<div className="flex justify-center items-center flex-col select-none text-white max-w-xl p-1 m-1 rounded-lg">
+				<span className="flex">Интересы:</span>
+
+				<div className="flex flex-wrap justify-center">
+					{payload.profile.interests.length ? (
+						payload.profile.interests.map((interest, i) => {
+							return (
+								<UserProfileInterest
+									key={interest + i}
+									value={interest}
+									title={interest}
+								/>
+							);
+						})
+					) : (
 						<UserProfileInterest
-							key={interest + i}
-							value={interest}
-							title={interest}
+							key={"interest" + payload.profile.userid}
+							value={"Отсутствуют"}
+							title={""}
 						/>
-					);
-				})
-			) : (
-				<UserProfileInterest
-					key={"interest" + payload.userId}
-					value={"Отсутствуют"}
-					title={""}
-				/>
-			)}
-		</>
+					)}
+				</div>
+			</div>
+		</div>
 	);
 }
