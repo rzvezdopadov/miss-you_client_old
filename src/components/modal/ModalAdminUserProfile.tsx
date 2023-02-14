0cc8ef036892a32131bd708917ae1dd5492d00d9
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-02-14 22:57:41 +0300

    integrated modal change ratting

--- src/components/modal/ModalAdminUserProfile.tsx
+++ src/components/modal/ModalAdminUserProfile.tsx
@@ -20,6 +20,10 @@ import { UserProfileAboutMe } from "../widgets/userprofile/UserProfileAboutMe";
 import { UserProfileNameAge } from "../widgets/userprofile/UserProfileNameAge";
 import { useQueryGetDialog } from "../../api/dialog/dialog.api.hook";
 import { IQueryDialog } from "../../api/dialog/idialog.api";
+import {
+	ModalAdminChangeRating,
+	modalAdminChangeRatingOpen,
+} from "./ModalAdminChangeRating";
 
 export function modalAdminUserProfileOpen(profile: IProfile) {
 	store.dispatch(userProfileAction(true, profile));
@@ -69,37 +73,43 @@ export function ModalAdminUserProfile() {
 	};
 
 	return (
-		<>
-			<div
-				ref={refUserProfile}
-				className="flex flex-col invisible fixed justify-start bg-gray-900 shadow-[0px_0px_5px_5px] shadow-lime-300 text-neutral-50 rounded-xl overflow-y-scroll lg:overflow-auto top-0 bottom-0 left-0 right-0 m-auto px-2 pt-2 z-20 pb-2 h-full lg:h-2/3 lg:max-w-5xl"
-			>
-				<ButtonModalClose onClick={closeUserProfileHandler} />
+		<div
+			ref={refUserProfile}
+			className="flex flex-col invisible fixed justify-start bg-gray-900 shadow-[0px_0px_5px_5px] shadow-lime-300 text-neutral-50 rounded-xl overflow-y-scroll lg:overflow-auto top-0 bottom-0 left-0 right-0 m-auto px-2 pt-2 z-20 pb-2 h-full lg:h-2/3 lg:max-w-5xl"
+		>
+			<ButtonModalClose onClick={closeUserProfileHandler} />
 
-				<div className="flex flex-wrap mt-4 flex-col lg:flex-row justify-center items-center h-fit w-full">
-					<div className="flex flex-col">
-						<UserProfileSlider />
-						<LabelRating value={userProfile.profile.rating} />
-						<LabelCash value={userProfile.profile.cash} />
-						<Button
-							value={"Написать сообщение"}
-							onClick={modalDialogOpenHandler}
-						/>
-					</div>
+			<div className="flex flex-wrap mt-4 flex-col lg:flex-row justify-center items-center h-fit w-full">
+				<div className="flex flex-col">
+					<UserProfileSlider />
+					<LabelRating
+						value={userProfile.profile.rating}
+						onClick={() => {
+							modalAdminChangeRatingOpen(
+								userProfile.profile.userid
+							);
+						}}
+					/>
+					<LabelCash value={userProfile.profile.cash} />
+					<Button
+						value={"Написать сообщение"}
+						onClick={modalDialogOpenHandler}
+					/>
+				</div>
 
-					<div className="flex items-center flex-col">
-						<UserProfileNameAge profile={userProfile.profile} />
+				<div className="flex items-center flex-col">
+					<UserProfileNameAge profile={userProfile.profile} />
 
-						<DateTimeVisit profile={userProfile.profile} />
-						<UserProfileAboutMe
-							discription={userProfile.profile.discription}
-						/>
-						<UserProfilePersonal profile={userProfile.profile} />
-						<UserProfileInterests profile={userProfile.profile} />
-						<UserProfileQuality profile={userProfile.profile} />
-					</div>
+					<DateTimeVisit profile={userProfile.profile} />
+					<UserProfileAboutMe
+						discription={userProfile.profile.discription}
+					/>
+					<UserProfilePersonal profile={userProfile.profile} />
+					<UserProfileInterests profile={userProfile.profile} />
+					<UserProfileQuality profile={userProfile.profile} />
 				</div>
 			</div>
-		</>
+			<ModalAdminChangeRating />
+		</div>
 	);
 }
