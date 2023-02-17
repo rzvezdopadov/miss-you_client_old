03e153424ae73109f6bbe58673520da3195d82e6
Roman Zvezdopadov <51131623+rzvezdopadov@users.noreply.github.com>
2023-02-17 08:47:31 +0300

    integrated input and select component

--- src/components/modal/ModalAdminBanned.tsx
+++ src/components/modal/ModalAdminBanned.tsx
@@ -1,21 +1,17 @@
 import * as React from "react";
 import { useEffect } from "react";
 import { store } from "../../store/store";
-import { userProfileAction } from "../../store/redusers/profile";
 import { modalMessageOpen } from "./ModalMessage";
 import { ModalYesCancelWrapper } from "../wrappers/ModalYesCancelWrapper";
-import {
-	useQuerySetAdminBanned,
-	useQuerySetAdminCash,
-} from "../../api/admin/admin.api.hook";
+import { useQuerySetAdminBanned } from "../../api/admin/admin.api.hook";
 import {
 	initialStateModalAdminBanned,
 	modalAdminBannedAction,
 } from "../../store/redusers/admin";
 import { ButtonsYesCancelWidget } from "../widgets/utils/Buttons";
 import { Input } from "../utils/Inputs";
-import { FiltersOptionAtArr } from "../utils/Selects";
-import { data_month } from "../../data/admin";
+import { SelectFromArr } from "../utils/Selects";
+import { data_hour, data_month } from "../../data/admin";
 
 export function modalAdminBannedOpen(userid: string) {
 	store.dispatch(
@@ -71,11 +67,51 @@ export function ModalAdminBanned() {
 			<>
 				<div className="flex">{`На сколько забанить пользователя c userid = "${modalAdminBanned.banned.userid}"?`}</div>
 				<Input
-					value={""}
-					onChange={() => {}}
+					value={modalAdminBanned.banned.discription}
+					onChange={(e) => {
+						const banned = { ...modalAdminBanned.banned };
+						banned.discription = e.target.value;
+						store.dispatch(modalAdminBannedAction(true, banned));
+					}}
 					type={"text"}
 					placeholder={"За что банить?"}
 				></Input>
+
+				<SelectFromArr
+					value={modalAdminBanned.banned.month}
+					keyOpt={"banned.month"}
+					onChangeHandler={(e) => {
+						const banned = { ...modalAdminBanned.banned };
+						banned.month = Number(e.target.value);
+						store.dispatch(modalAdminBannedAction(true, banned));
+					}}
+					arr={data_month}
+					title={"Месяцев"}
+				/>
+
+				<SelectFromArr
+					value={modalAdminBanned.banned.hour}
+					keyOpt={"banned.hour"}
+					onChangeHandler={(e) => {
+						const banned = { ...modalAdminBanned.banned };
+						banned.hour = Number(e.target.value);
+						store.dispatch(modalAdminBannedAction(true, banned));
+					}}
+					arr={data_hour}
+					title={"Часов"}
+				/>
+
+				<SelectFromArr
+					value={modalAdminBanned.banned.minute}
+					keyOpt={"banned.minute"}
+					onChangeHandler={(e) => {
+						const banned = { ...modalAdminBanned.banned };
+						banned.minute = Number(e.target.value);
+						store.dispatch(modalAdminBannedAction(true, banned));
+					}}
+					arr={data_hour}
+					title={"Минут"}
+				/>
 
 				<ButtonsYesCancelWidget
 					onClickYes={yesModalAdminBannedHandler}
