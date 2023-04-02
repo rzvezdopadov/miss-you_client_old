2859f3ecaeb75296727b0b2cc0efed9edf7c8607
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-04-02 14:42:27 +0300

    added new property

--- src/interfaces/iprofiles.ts
+++ src/interfaces/iprofiles.ts
@@ -72,6 +72,7 @@ export interface IProfile {
 	location: string;
 	likes: string[];
 	favoriteusers: string[];
+	bannedusers: string[];
 	birthday: number;
 	monthofbirth: number;
 	yearofbirth: number;
