3c3bcfc8e38e2a2ebc41d8dd9eceae3e49479b65
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-05-19 22:04:45 +0300

    integrated new component

--- src/role_all/components/widgets/settingprofile/SettingProfileAbout.tsx
+++ src/role_all/components/widgets/settingprofile/SettingProfileAbout.tsx
@@ -1,11 +1,16 @@
 import * as React from "react";
-import { LabelCash, LabelRating } from "../../utils/Labels";
+import { Label, LabelCash, LabelRating } from "../../utils/Labels";
 import { onChangeValueProfile } from "../../../helpers/profile";
 import { Input } from "../../utils/Inputs";
 import { storeAll } from "../../../store/storeAll";
+import { BG_COLOR } from "../../../../assets/styles/enum";
+import { convertTextToSign } from "../../../helpers/convert";
+import { WidgetWrapper } from "../../wrappers/WidgetWrapper";
 
 export function SettingProfileAbout() {
 	const { userMyProfile } = storeAll.getState();
+	const refererlink = `${window.location.protocol}//${window.location.host}/registration?userid=${userMyProfile.userid}`;
+	const refererDiscription = `За каждого приведенного участника сайта, начислим 5% MY-баллов от каждого его пополнения счета!`;
 
 	const nameOnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
 		onChangeValueProfile(e, "name", "string");
@@ -17,9 +22,32 @@ export function SettingProfileAbout() {
 	};
 
 	return (
-		<div className="flex flex-col w-full">
+		<WidgetWrapper wrap={true} col={true} itemsCenterDisable={true}>
 			<LabelCash value={userMyProfile.cash} />
 			<LabelRating value={userMyProfile.rating} />
+
+			<Label
+				value={`Ваш Id в системе: '${userMyProfile.userid}'`}
+				shadow={true}
+				selectauto={true}
+				bgcolor={BG_COLOR.Lime700}
+			/>
+
+			<WidgetWrapper wrap={true} col={true} itemsCenterDisable={true}>
+				<Label
+					title={`За каждого приведенного участника сайта, начислим 5% MY-баллов от каждого его пополнения счета!`}
+				>
+					{`Ваша реферальская ссылка:`} {convertTextToSign("&nbsp")}
+				</Label>
+				<a
+					href={refererlink}
+					target="_blank"
+					style={{ color: `lightgreen` }}
+				>
+					{refererlink}
+				</a>
+			</WidgetWrapper>
+
 			<Input
 				value={userMyProfile.name}
 				onChange={nameOnChangeHandler}
@@ -32,10 +60,10 @@ export function SettingProfileAbout() {
 					value={userMyProfile.discription}
 					onChange={discriptionOnChangeHandler}
 					title="О себе"
-					className="flex text-center resize-none rounded-md shadow-[0px_0px_3px_3px] shadow-lime-300 bg-slate-300 text-black m-1"
+					className="flex text-center resize-none rounded-md shadow-[0px_0px_3px_3px] shadow-lime-300 bg-slate-300 text-black m-0.5"
 					placeholder="О себе"
 				></textarea>
 			</div>
-		</div>
+		</WidgetWrapper>
 	);
 }
