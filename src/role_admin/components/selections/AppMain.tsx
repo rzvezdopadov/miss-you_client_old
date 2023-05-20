46ceeea9bec8ce0a7f6e41390fb2066c0002de0c
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-05-20 13:54:40 +0300

    change routs

--- src/role_admin/components/selections/AppMain.tsx
+++ src/role_admin/components/selections/AppMain.tsx
@@ -9,19 +9,21 @@ import { userMyProfileAction } from "../../../role_all/store/redusers/profile";
 import { modalMessageOpen } from "../../../role_all/components/modal/ModalMessage";
 import { stickerpacksAction } from "../../../role_all/store/redusers/stickerpacks";
 import { MainWrapper } from "../../../role_all/components/wrappers/MainWrapper";
-import { RoutesAll } from "../../../role_all/routes/RoutesAll";
-import { RoutesAllAuth } from "../../../role_all/routes/RoutesAllAuth";
-import { RoutesNoAuth } from "../../../role_all/routes/RoutesNoAuth";
-import { RoutesLocal } from "../../routes/RoutesLocal";
 import { store } from "../../store/store";
 import { Dialogs } from "../pages/Dialogs";
 import { SettingProfile } from "../../../role_all/components/pages/SettingProfile";
 import { Logout } from "../../../role_all/components/pages/Logout";
 import { useQueryGetTowns } from "../../../role_all/api/towns/towns.api.hook";
 import { townsAction } from "../../../role_all/store/redusers/towns";
+import { Statistics } from "../pages/Statistics";
+import { Profiles } from "../pages/Profiles";
+import { Login } from "../../../role_all/components/pages/Login";
+import { RecoveryPass } from "../../../role_all/components/pages/RecoveryPass";
+import { Registration } from "../../../role_all/components/pages/Registration";
 
 export function AppMain() {
 	const { jwt, userMyProfile } = storeAll.getState();
+	const { userFilters } = store.getState();
 	const { dataGetProfile, errorGetProfile, querySendGetProfile } =
 		useQueryGetProfile();
 	const { dataStickerpacks, errorStickerpacks, querySendGetStickerpacks } =
@@ -37,15 +39,15 @@ export function AppMain() {
 
 			querySendGetProfile(data);
 			querySendGetStickerpacks();
-			useQueryGetTowns();
 		}
+
+		querySendGetTowns();
 	}, [jwt]);
 
 	useEffect(() => {
 		if (!dataGetProfile) return;
 
-		store.dispatch(filtersUserAction(dataGetProfile.filters));
-		store.dispatch(userMyProfileAction(dataGetProfile));
+		storeAll.dispatch(userMyProfileAction(dataGetProfile));
 	}, [dataGetProfile]);
 
 	useEffect(() => {
@@ -70,6 +72,10 @@ export function AppMain() {
 		if (!dataGetTowns) return;
 
 		storeAll.dispatch(townsAction(dataGetTowns));
+
+		const newUserFilters = { ...userFilters };
+		newUserFilters.location = dataGetTowns[0];
+		store.dispatch(filtersUserAction(newUserFilters));
 	}, [dataGetTowns]);
 
 	useEffect(() => {
@@ -87,14 +93,38 @@ export function AppMain() {
 				{jwt ? (
 					userMyProfile.userid !== "" ? (
 						<>
-							<RoutesAllAuth />
-							<RoutesLocal />
+							<Route path="/dialogs" element={<Dialogs />} />
+							<Route
+								path="/settings"
+								element={<SettingProfile />}
+							/>
+							<Route path="/logout" element={<Logout />} />
+							<Route
+								path="/userprofiles"
+								element={<Profiles />}
+							/>
+							<Route
+								path="/statistics"
+								element={<Statistics />}
+							/>
+							<Route path="/*" element={<Statistics />} />
 						</>
 					) : (
 						<></>
 					)
 				) : (
-					<RoutesNoAuth />
+					<>
+						<Route path="/login" element={<Login />} />
+						<Route
+							path="/recoverypass"
+							element={<RecoveryPass />}
+						/>
+						<Route
+							path="/registration"
+							element={<Registration />}
+						/>
+						<Route path="/*" element={<Registration />} />
+					</>
 				)}
 			</Routes>
 		</MainWrapper>
