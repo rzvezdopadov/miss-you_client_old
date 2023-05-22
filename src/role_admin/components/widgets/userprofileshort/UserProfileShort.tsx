7dea8b688d6bd16dca7dd7ab7d683b7d6a0cd58b
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-05-22 14:33:59 +0300

    change component

--- src/role_admin/components/widgets/userprofileshort/UserProfileShort.tsx
+++ src/role_admin/components/widgets/userprofileshort/UserProfileShort.tsx
@@ -40,10 +40,9 @@ export function UserProfileShort(payload: { profile: IProfileShort }) {
 		<div className="flex justify-center shadow-[0px_0px_1px_1px] shadow-lime-300 flex-row bg-gray-900 text-neutral-50 rounded-xl m-2 px-2 pt-2 pb-2 max-h-52 w-80">
 			<div className="flex flex-col justify-center">
 				<PhotoProfileShort src={payload.profile.photolink} />
-				<Button value={"Посмотреть"} onClick={openProfileHandler} />
-				<UserProfileInterest
+				<Button
 					value={`"${payload.profile.userid}"`}
-					title={`"${payload.profile.userid}"`}
+					onClick={openProfileHandler}
 				/>
 				<DateTimeVisitShort profile={payload.profile} />
 			</div>
