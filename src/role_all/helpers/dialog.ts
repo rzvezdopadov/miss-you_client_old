c3a9a71c70b4bbe6bfda1e637915a4e8892bb0b4
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-06-03 12:38:13 +0300

    fix sorting

--- src/role_all/helpers/dialog.ts
+++ src/role_all/helpers/dialog.ts
@@ -1,10 +1,22 @@
 import { IDialog } from "../interfaces/iprofiles";
 
 export function dialogsSort(dialogs: IDialog[]) {
-	// dialogs.sort((a, b) => b.msgs[0].timecode - a.msgs[0].timecode);
-	// dialogs.forEach((dialog) =>
-	// 	dialog.msgs.sort((a, b) => a.timecode - b.timecode)
-	// );
+	if (!dialogs) return [];
 
-	return dialogs;
+	const newDialogs = dialogs
+		.map((dialog) => {
+			const newDialog = { ...dialog };
+			const newMessages = [...newDialog.msgs];
+			newDialog.msgs = newMessages.sort(
+				(a, b) => a.timecode - b.timecode
+			);
+			return newDialog;
+		})
+		.sort(
+			(a, b) =>
+				b.msgs[b.msgs.length - 1].timecode -
+				a.msgs[a.msgs.length - 1].timecode
+		);
+
+	return newDialogs;
 }
