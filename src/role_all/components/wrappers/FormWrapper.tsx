bca8e96dfe756ce797433aafafea04be0b743956
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-06-04 12:42:53 +0300

    change layout

--- src/role_all/components/wrappers/FormWrapper.tsx
+++ src/role_all/components/wrappers/FormWrapper.tsx
@@ -1,6 +1,6 @@
 export function FormWrapper(payload: { children: React.ReactNode }) {
 	return (
-		<div className="flex flex-col bg-gray-700 shadow-[0px_0px_2px_2px] shadow-lime-300 rounded-xl px-2 pt-2 pb-2 w-80 overflow-scroll">
+		<div className="flex flex-col bg-gray-900 shadow-[0px_0px_2px_2px] shadow-lime-300 rounded-xl px-2 pt-2 pb-2 w-80 overflow-scroll">
 			{payload.children}
 		</div>
 	);
