035ca9650b640c543e095cda66534aae003bc79d
Roman Zvezdopadov <rzvezdopadov@gmail.com>
2023-06-04 12:43:57 +0300

    added label for discription

--- src/role_all/components/utils/Inputs.tsx
+++ src/role_all/components/utils/Inputs.tsx
@@ -8,15 +8,25 @@ export function Input(payload: {
 	placeholder: string;
 }) {
 	return (
-		<div key={payload.placeholder} className="flex flex-col my-1">
+		<div
+			key={payload.placeholder}
+			className="flex relative flex-col my-1 p-1"
+		>
 			<input
 				value={payload.value}
 				onChange={payload.onChange}
 				title={payload.placeholder}
-				className="flex text-center rounded-md shadow-[0px_0px_2px_2px] shadow-lime-300 bg-slate-300 text-black m-0.5 py-1 px-3"
+				className="flex text-center rounded-md border-2 border-lime-300  bg-inherit text-white m-0.5 py-1 px-3"
 				type={payload.type}
 				placeholder={payload.placeholder}
 			/>
+			<label
+				className={`flex justify-center items-center absolute transition-opacity ease-linear delay-500 text-center text-[10px] h-0 bg-gray-900 text-white -translate-y-2 translate-x-2 py-2 px-3${
+					!payload.value ? " opacity-0" : " opacity-100"
+				}`}
+			>
+				{payload.placeholder}
+			</label>
 		</div>
 	);
 }
